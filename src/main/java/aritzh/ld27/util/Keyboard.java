2131bc642621772cea2b9e03ba93732931b48669
aritzhack <aritzhack@gmail.com>
2013-09-07 17:46:41 +0200

    + Added documentation to the Keyboard class + Added a method to check if a specific key is being pressed at the moment + Added numeric pad arrow keys to the helper methods + Put a region around the listener interface methods, for easier folding
    * Reorganized, so that reset methods are together

--- src/main/java/aritzh/ld27/util/Keyboard.java
+++ src/main/java/aritzh/ld27/util/Keyboard.java
@@ -8,15 +8,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
+ * Keyboard and focus handler.
  * @author Aritz Lopez
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
 public class Keyboard implements KeyListener, FocusListener {
 
-    boolean focus = true;
-    boolean[] keys = new boolean[Character.MAX_VALUE];
-    ArrayList<Integer> checked = new ArrayList<Integer>();
+    private boolean focus = true;
+    private boolean[] keys = new boolean[Character.MAX_VALUE];
+    private ArrayList<Integer> checked = new ArrayList<Integer>();
 
+    // region ...Listener methods...
     /**
      * Invoked when a component gains the keyboard focus.
      */
@@ -66,6 +68,16 @@ public class Keyboard implements KeyListener, FocusListener {
             if (this.checked.get(i).equals(e.getKeyCode())) this.checked.remove(i);
     }
 
+    // endregion
+
+    /**
+     * Checks whether the specified key was typed (pressed, but not checked if it was pressed)
+     * <br />
+     * Example: {@code isKeyTyped(KeyEvent.VK_F)} to check whether the key F was pressed since the last call to this method
+     * @param key The key code to check.
+     * @return true if the key hasn't been checked since it was last pressed, false otherwise
+     * @see KeyEvent KeyEvent for the keycodes
+     */
     public boolean isKeyTyped(int key) {
         if (this.keys[key] && !this.checked.contains(key)) {
             this.checked.add(key);
@@ -74,30 +86,69 @@ public class Keyboard implements KeyListener, FocusListener {
         return false;
     }
 
-    public void resetChecked() {
-        this.checked.clear();
+    /**
+     * Returns true if the key is currently pressed
+     * @param key The key code
+     * @return true if the key is currently pressed
+     * @see KeyEvent KeyEvent for the keycodes
+     */
+    public boolean isKeyDown(int key){
+        return this.keys[key];
     }
 
+    /**
+     * Whether {@link KeyEvent#VK_UP} or {@link KeyEvent#VK_KP_UP} or {@link KeyEvent#VK_W} are currently pressed
+     * @return true if any up-meaning key are pressed
+     */
     public boolean isUp() {
-        return this.keys[KeyEvent.VK_W] || this.keys[KeyEvent.VK_UP];
+        return this.keys[KeyEvent.VK_W] || this.keys[KeyEvent.VK_UP] || this.keys[KeyEvent.VK_KP_UP];
     }
 
+    /**
+     * Whether {@link KeyEvent#VK_DOWN} or {@link KeyEvent#VK_KP_DOWN} or {@link KeyEvent#VK_S} are currently pressed
+     * @return true if any down-meaning key are pressed
+     */
     public boolean isDown() {
-        return this.keys[KeyEvent.VK_S] || this.keys[KeyEvent.VK_DOWN];
+        return this.keys[KeyEvent.VK_S] || this.keys[KeyEvent.VK_DOWN] || this.keys[KeyEvent.VK_KP_DOWN];
     }
 
+    /**
+     * Whether {@link KeyEvent#VK_LEFT} or {@link KeyEvent#VK_KP_LEFT} or {@link KeyEvent#VK_A} are currently pressed
+     * @return true if any left-meaning key are pressed
+     */
     public boolean isLeft() {
-        return this.keys[KeyEvent.VK_A] || this.keys[KeyEvent.VK_LEFT];
+        return this.keys[KeyEvent.VK_A] || this.keys[KeyEvent.VK_LEFT] || this.keys[KeyEvent.VK_KP_LEFT];
     }
 
+    /**
+     * Whether {@link KeyEvent#VK_RIGHT} or {@link KeyEvent#VK_KP_RIGHT} or {@link KeyEvent#VK_D} are currently pressed
+     * @return true if any right-meaning key are pressed
+     */
     public boolean isRight() {
-        return this.keys[KeyEvent.VK_D] || this.keys[KeyEvent.VK_RIGHT];
+        return this.keys[KeyEvent.VK_D] || this.keys[KeyEvent.VK_RIGHT] || this.keys[KeyEvent.VK_KP_RIGHT];
     }
 
+    /**
+     * True if the window this was registered to has currently focus
+     * @return true if the window has focus
+     */
     public boolean hasFocus() {
         return this.focus;
     }
 
+    /**
+     * Resets all the checked keys. Effectively making all keys unchecked, so if the key is pressed, calling {@link Keyboard#isKeyTyped(int)} will return true
+     */
+    public void resetChecked() {
+        this.checked.clear();
+    }
+
+    /**
+     * Resets a specific key. For later use with {@link Keyboard#isKeyTyped(int)}
+     * @param keyCode The key code to reset
+     * @see aritzh.ld27.util.Keyboard#resetChecked()
+     * @see KeyEvent KeyEvent for the keycodes
+     */
     public void resetKey(int keyCode) {
         this.checked.remove(Integer.valueOf(keyCode));
     }
